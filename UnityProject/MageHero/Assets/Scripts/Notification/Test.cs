6f415535dfa7621cdb707277e14c519f722a3415
Marsik-dev <marselkuznetsov@yandex.ru>
2020-09-16 18:25:44 +0300

    0.0.3.1
    add test notification

---
+++ UnityProject/MageHero/Assets/Scripts/Notification/Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Unity.Notifications.Android;
+using UnityEngine.EventSystems;
+
+public class Test : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+        print("121");
+
+        var notification = new AndroidNotification();
+        notification.Title = "Test Title";
+        notification.Text = "Test messadge";
+        notification.FireTime = System.DateTime.Now.AddMinutes(0.1f);
+
+        AndroidNotificationCenter.SendNotification(notification, "Test");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        AndroidNotificationChannel channel = new AndroidNotificationChannel("Test", "Mage Hero", "It's a test notification", Importance.Default);
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+
+    }
+
+    
+
+
+}
